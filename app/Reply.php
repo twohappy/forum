896e5f237b8a480a345217fa08ebfe6254486cdf
twohappy <autumnyout@gmail.com>
2017-07-04 23:12:38 +0800

    activity feed now includes favorite

--- app/Reply.php
+++ app/Reply.php
@@ -22,4 +22,8 @@ class Reply extends Model
         return $this->belongsTo(Thread::class);
     }
 
+    public function path()
+    {
+        return $this->thread->path() . "#reply-{$this->id}";
+    }
 }
